607641cfde83b36d799b71df420ea7f72cb09389
Tomas Pavlic <tomas.pavlic@aps-holding.com>
2020-04-16 20:11:37 +0200

    Add project files

---
+++ Helpers/MessageParser.cs
@@ -0,0 +1,34 @@
+using System.Text.RegularExpressions;
+using Topdev.Twitch.Chat.Models;
+
+namespace Topdev.Twitch.Chat.Helpers
+{
+    class MessageParser
+    {
+        /// <summary>
+        /// Tries to parse raw message into message object.
+        /// </summary>
+        /// <param name="message">Raw message received from a server</param>
+        /// <param name="msg">Output message object when successfully parsed</param>
+        /// <returns></returns>
+        public bool TryParsePrivateMessage(string message, out Message msg)
+        {
+            msg = new Message();
+            var regex = new Regex(":(?<user>.*)!(.*)@(?<host>.*) PRIVMSG #(?<channel>.*) :(?<text>.*)");
+            var match = regex.Match(message);
+
+            if (!match.Success)
+                return false;
+            
+            var groups = match.Groups;
+
+            msg.RawMessage = message;
+            msg.User = groups["user"].Value;
+            msg.Host = groups["host"].Value;
+            msg.Channel = groups["channel"].Value;
+            msg.Text = groups["text"].Value;
+
+            return true;
+        }
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,15 @@
+namespace Topdev.Twitch.Chat.Models
+{
+    public class Message
+    {
+        public string RawMessage { get; set; }
+
+        public string User { get; set; }
+
+        public string Host { get; set; }
+
+        public string Channel { get; set; }
+        
+        public string Text { get; set; }
+    }
+}
